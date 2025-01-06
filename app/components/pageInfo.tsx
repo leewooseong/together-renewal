cfe674c65b2305b182cee330ba943c4c56987176
eundin <escience1202@gmail.com>
2025-01-06 17:39:34 +0900

    Feat SCRUM-84: pageInfo 컴포넌트 구현 - 모임 찾기, 찜한 모임, 모든 리뷰 페이지에 공통으로 사용되는 페이지 소개 컴포넌트 구현

---
+++ app/components/pageInfo.tsx
@@ -0,0 +1,25 @@
+interface IpageInfoType {
+    // 모임찾기 페이지 = "gatherings" | 모든 리뷰 페이지 = "reviews" | 찜한 모임 페이지 = "likes"
+    pageName: "gatherings" | "reviews" | "likes";
+}
+
+export default function PageInfo({ pageName }: IpageInfoType) {
+    return (
+        <div className={`flex items-center gap-4 xs:gap-[7px]`}>
+            <img src={`/${pageName}-page-head.png`} alt="연필 모양 이미지" />
+            <div className={`flex  gap-2 h-[60px] ${pageName === "gatherings" ? "flex-col-reverse" : "flex-col"}`}>
+                <h1 className="text-lg font-semibold text-gray-900 xs:text-2xl">
+                    {pageName === "gatherings" && "지금 모임에 참여해보세요"}
+                    {pageName === "reviews" && "모든 리뷰"}
+                    {pageName === "likes" && "찜한 모임"}
+                </h1>
+                <p className="text-sm font-medium text-gray-700">
+                    {pageName === "gatherings" && "함께 할 사람이 없나요?"}
+                    {pageName === "reviews" && "같이 달램을 이용한 분들은 이렇게 느꼈어요😉"}
+                    {pageName === "likes" && "마감되기 전에 지금 바로 참여해보세요🤓"}
+                </p>
+            </div>
+        </div>
+    );
+}
+
