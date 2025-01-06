ad8026e13e306e095762feb8a44aa07e338b8178
eundin <escience1202@gmail.com>
2025-01-06 18:03:35 +0900

    Feat SCRUM-84: IpageInfoType 타입 분리, 리뷰 공통 컴포넌트 반응형 사이즈 수정

--- app/components/pageInfo.tsx
+++ app/components/pageInfo.tsx
@@ -1,7 +1,4 @@
-interface IpageInfoType {
-    // 모임찾기 페이지 = "gatherings" | 모든 리뷰 페이지 = "reviews" | 찜한 모임 페이지 = "likes"
-    pageName: "gatherings" | "reviews" | "likes";
-}
+import { IpageInfoType } from "../types/pageInfo";
 
 export default function PageInfo({ pageName }: IpageInfoType) {
     return (
