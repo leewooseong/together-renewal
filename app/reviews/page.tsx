01df8868c73761bb34a01351a777b8d33b568a8c
eundin <escience1202@gmail.com>
2025-01-06 13:15:27 +0900

    Feat SCRUM-11: 모든 리뷰 페이지 로딩 상태 처리 - 로딩시 보여줄 UI 구현 필요

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-
+// 나중에 모든 리뷰 페이지 구현 해야함.
 import { useQuery } from "@tanstack/react-query";
 import { getReviews } from "../apis/reviews";
 import Review from "../components/reviewComponent/review";
@@ -7,14 +7,19 @@ import { IReviews } from "../types/reviews";
 
 
 export default function RivewsPage({ userId, gatheringId }: { userId?: number; gatheringId?: number }) {
-    const { data: reviews } = useQuery<IReviews>({
+    const { data: reviews, isError, isPending } = useQuery<IReviews>({
         queryKey: ["reviews", { userId, gatheringId }],
         queryFn: () => getReviews(userId, gatheringId),
         staleTime: 1 * 60 * 5000, //5분
         gcTime: 60 * 1000 * 10, //10분
-
     })
 
+
+    if (isPending) {
+        console.log("로딩중...")
+        return
+
+    }
     //tanstack 적용 전
     // const [reviews, setReviews] = useState<IReviews["data"]>([])
 
