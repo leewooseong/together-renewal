0e74f838b044f3c682106b682d03f93c21bee617
gunhee0000 <132315752+gunhee0000@users.noreply.github.com>
2025-01-08 16:00:46 +0900

    Rename/#SCRUM-90-mypage
    마이페이지 및 컴포넌트 파일 이름 변경

--- app/components/mypageComponent/myPageCard.tsx
+++ app/components/mypageComponent/myPageCard.tsx
@@ -17,7 +17,7 @@ interface MyPageCardProps extends IGetJoinedGatherings {
 }
 
 /** 마이페이지 - 나의모임 card */
-export default function MyPageCard({isMyGathering, ...param}: MyPageCardProps) {
+export default function MyPageCard({isMyGathering, ...props}: MyPageCardProps) {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const RenderButton: React.FC<{state: boolean; review: boolean}> = ({state, review}) => {
@@ -33,7 +33,7 @@ export default function MyPageCard({isMyGathering, ...param}: MyPageCardProps) {
         if (state) {
           setIsModalOpen(true);
         } else {
-          await leaveGatheringsApi(param.id);
+          await leaveGatheringsApi(props.id);
           window.location.href = '/mypage';
         }
       } catch {
@@ -75,19 +75,19 @@ export default function MyPageCard({isMyGathering, ...param}: MyPageCardProps) {
   };
 
   function isCanceled() {
-    if (param.canceledAt) return RenderOverlay('모집 취소', '[328px]', param.id);
-    if (new Date(param.dateTime) < new Date()) return RenderOverlay('마감', '[328px]', param.id);
+    if (props.canceledAt) return RenderOverlay('모집 취소', '[328px]', props.id);
+    if (new Date(props.dateTime) < new Date()) return RenderOverlay('마감', '[328px]', props.id);
     return null;
   }
 
-  const dateFormat = formatDateUtil(param.dateTime);
+  const dateFormat = formatDateUtil(props.dateTime);
 
   return (
     <div className="relative flex flex-col sm:flex-col justify-between w-[311px] sm:w-full max-w-[996px] h-[352px] sm:h-[180px]">
       {/* 리뷰 작성 모달 */}
       {isModalOpen && (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-          <WriteReviewModal onClose={() => setIsModalOpen(false)} gatheringId={param.id} />
+          <WriteReviewModal onClose={() => setIsModalOpen(false)} gatheringId={props.id} />
         </div>
       )}
 
@@ -97,21 +97,21 @@ export default function MyPageCard({isMyGathering, ...param}: MyPageCardProps) {
       <div className="flex flex-col justify-between sm:flex-row w-full h-[328px] sm:w-[545px] sm:h-[156px]">
         {/* 모임 이미지 */}
         <div className="sm:w-[280px] w-full h-full flex items-center justify-center overflow-hidden border border-dashed rounded-3xl">
-          <img src={param.image} alt="모임 대표 이미지" className="object-cover" />
+          <img src={props.image} alt="모임 대표 이미지" className="object-cover" />
         </div>
 
         <div className="w-[249px] h-full flex flex-col justify-between">
           {/* 상태 Chips */}
-          {RenderChips(param.isCompleted, param.participantCount, isMyGathering)}
+          {RenderChips(props.isCompleted, props.participantCount, isMyGathering)}
 
           {/* 모임 정보 */}
           <div className="w-full h-[54px] flex flex-col justify-between">
             {/* 모임 이름, 장소 */}
             <div className="h-[28px] flex justify-between items-center">
-              <div className="w-[170px] font-semibold overflow-hidden truncate">{param.name}</div>
+              <div className="w-[170px] font-semibold overflow-hidden truncate">{props.name}</div>
               <div className="w-[76px] flex justify-between">
                 <span className="font-semibold">{'|'}</span>
-                <p className="font-medium text-sm">{param.location}</p>
+                <p className="font-medium text-sm">{props.location}</p>
               </div>
             </div>
 
@@ -120,14 +120,14 @@ export default function MyPageCard({isMyGathering, ...param}: MyPageCardProps) {
               <span>{`${dateFormat.date} ${dateFormat.time}`}</span>
               <span className="flex gap-[4px]">
                 <img src="/personIcon.svg" className="pt-0.5" />
-                {`${param.participantCount}/${param.capacity}`}
+                {`${props.participantCount}/${props.capacity}`}
               </span>
             </div>
           </div>
 
           {/* 취소, 리뷰 버튼 */}
           <div className="h-[40px] mt-[12px]">
-            <RenderButton state={param.isCompleted} review={param.isReviewed} />
+            <RenderButton state={props.isCompleted} review={props.isReviewed} />
           </div>
         </div>
       </div>
