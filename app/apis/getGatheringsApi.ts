5c7398ae1110665165e041c04a4cad2bee94b1a3
gunhee0000 <132315752+gunhee0000@users.noreply.github.com>
2025-01-08 14:37:57 +0900

    feat/SCRUM-90-마이페이지 구현
    -컨벤션 맞춰서 일부 파일 수정
    -myPage 일부 구현(미완성)
    -컴포넌트 일부 동작 수정

--- app/apis/getGatheringsApi.ts
+++ app/apis/getGatheringsApi.ts
@@ -1,6 +1,6 @@
 import {BASE_API_URL, TEAM_ID} from './../constants/commonConstants';
 export async function getGatheringApi(): Promise<IGetGatherings[]> {
-  const resp = await fetch(`${BASE_API_URL}/${TEAM_ID}gatherings`);
+  const resp = await fetch(`${BASE_API_URL}/${TEAM_ID}/gatherings`);
 
   if (!resp.ok) {
     throw new Error('Failed to fetch gatherings');
