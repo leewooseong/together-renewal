9692971c2fda0d69f5829a8e2ad0eec5d0a8937b
이우성 <42796944+leewooseong@users.noreply.github.com>
2025-01-09 17:14:52 +0900

    Fix SCRUM-3: main page에서 use client 사용해서 발생하는 에러 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,4 @@
-'use client';
-
-import {useEffect} from 'react';
-
-import {useAtom} from 'jotai';
-
 import {Metadata} from 'next';
-import {getUserInfo} from './apis/user/userApi';
-import LogoutButton from './components/logoutButton';
-import {userInfoAtom} from './store/atoms/userAtoms';
 
 export const metadata: Metadata = {
   title: '서비스 명',
@@ -15,21 +6,9 @@ export const metadata: Metadata = {
 };
 
 export default function Home() {
-  const [userInfo, setUserInfo] = useAtom(userInfoAtom);
-  // const [, setToken] = useAtom(tokenWithStorageAtom);
-  const fetchUserInfo = async () => {
-    const userInfoData = await getUserInfo();
-    setUserInfo(userInfoData);
-  };
-  useEffect(() => {
-    // setToken(null); // api 요청시 token이 빠져있을 경우 테스트를 위한 코드
-    fetchUserInfo();
-  }, []);
   return (
     <div>
-      {userInfo && <p>{userInfo.name}</p>}
       <p>메인 페이지</p>
-      <LogoutButton />
     </div>
   );
 }
