92adaaaded1e39e33ee2e1f22f1d938c7d8d8ea9
이우성 <42796944+leewooseong@users.noreply.github.com>
2025-01-13 10:03:32 +0900

    Feat SCRUM-95: 불필요한 api 서버로의 요청을 막기위한 api 요청 middleware 작성

--- app/types/error.types.ts
+++ app/types/error.types.ts
@@ -2,8 +2,9 @@
 type AuthErrorMessage =
   | '비밀번호가 아이디와 일치하지 않습니다.'
   | '존재하지 않는 아이디입니다.'
-  | '서버 오류가 발생했습니다.';
-type AuthErrorCode = 'INVALID_CREDENTIALS' | 'USER_NOT_FOUND' | 'SERVER_ERROR';
+  | '서버 오류가 발생했습니다.'
+  | '잘못된 인증 정보 입니다.';
+type AuthErrorCode = 'INVALID_CREDENTIALS' | 'USER_NOT_FOUND' | 'SERVER_ERROR' | 'INVALID_TOKEN';
 type AuthErrorStatus = 401 | 404 | 500;
 
 export class AuthError extends Error {
--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,11 @@
 import {cookies} from 'next/headers';
-import {NextResponse, type NextRequest} from 'next/server';
+import {type NextRequest} from 'next/server';
 
 import {AUTH_TOKEN} from '@/app/constants/auth';
 import getRequestType from '@/app/utils/server';
 
 import {isPageType} from './app/utils/type';
+import {handleApiRequest} from './middlewares/apiHandler';
 import {protectRoute} from './middlewares/routeProtection';
 
 export default async function middleware(request: NextRequest) {
@@ -12,14 +13,12 @@ export default async function middleware(request: NextRequest) {
   const token = cookieStore.get(AUTH_TOKEN);
 
   const requestType = getRequestType(request.nextUrl.pathname);
+
   if (isPageType(requestType)) {
     return protectRoute(token, requestType, request.url);
   }
 
-  // 추가적인 middleware 선언은 아래서 처리
-  // ...
-
-  return NextResponse.next();
+  return handleApiRequest(token, requestType);
 }
 
 // 변수로 선언하면 반영이 되지 않으니 주의!
