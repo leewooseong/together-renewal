8a99808d2393d12ec97be24b0acc015a42dee7a2
eundin <escience1202@gmail.com>
2025-01-16 17:44:42 +0900

    Chore: reviews의 route.ts파일에 상수 변수명을 capital로 변경

--- app/route/reviews/route.ts
+++ app/route/reviews/route.ts
@@ -1,18 +1,18 @@
 import {NextRequest, NextResponse} from 'next/server';
 
 export async function GET(request: NextRequest) {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-  const teamId = process.env.NEXT_PUBLIC_TEAM_ID;
+  const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+  const TEAM_ID = process.env.NEXT_PUBLIC_TEAM_ID;
   const {searchParams} = new URL(request.url);
 
-  const userId = searchParams.get('userId');
-  const gatheringId = searchParams.get('gatheringId');
-  const sortOrder = searchParams.get('sortOrder');
-  const gatheringType = searchParams.get('type');
-  console.log('Request URL:', request.url);
-  console.log('Search Params:', searchParams.toString());
-  console.log(userId, gatheringId, sortOrder, gatheringType);
-  const response = await fetch(`${baseUrl}/${teamId}/reviews?${searchParams}`);
+  // const userId = searchParams.get('userId');
+  // const gatheringId = searchParams.get('gatheringId');
+  // const sortOrder = searchParams.get('sortOrder');
+  // const gatheringType = searchParams.get('type');
+  // console.log('Request URL:', request.url);
+  // console.log('Search Params:', searchParams.toString());
+  // console.log(userId, gatheringId, sortOrder, gatheringType);
+  const response = await fetch(`${BASE_URL}/${TEAM_ID}/reviews?${searchParams}`);
   const data = await response.json();
   return NextResponse.json(data);
 }
