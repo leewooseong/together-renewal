2cac20d89dc1757efca72ac033606d03373ba9c8
gunhee0000 <132315752+gunhee0000@users.noreply.github.com>
2025-02-12 16:00:15 +0900

    fix/#SCRUM-178-resolve-confilct
    - 모임 정렬 안되는 현상 수정

--- app/queries/common/queryKeys.ts
+++ app/queries/common/queryKeys.ts
@@ -11,6 +11,7 @@ import {
   GetMyReviewsProps,
   GetReviewsProps,
 } from '../../types/reviews/reviewsApi.types';
+import {myGatheringSort} from '../../utils/myGatheringSort';
 
 // 'info'와 같이 callback 함수로 작성하면 매개변수를 받아 매개변수에 맞는 queryKey를 생성할 수 있다.
 export const userQueryKey = {
@@ -41,7 +42,7 @@ export const gatheringsQueryKey = {
   all: ['gatherings'] as const,
   joinedGatherings: () => ({
     queryKey: ['joinedGatherings'] as const,
-    queryFn: () => getJoinedGatherings(),
+    queryFn: async () => myGatheringSort(await getJoinedGatherings()),
   }),
   GatheringDetails: (id: number) => ['gathering', id] as const,
   gatheringParticipants: (id: number) => ({
